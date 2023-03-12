8605921f6e2ca247fee9ad1213e731e664d20aa5
BryanMarc01 <bm7068959@gmail.com>
2023-03-11 23:08:20 -0400

    Base inicial - Login

---
+++ Proyecto - Sistema Inscripcion/procesar_login.php
@@ -0,0 +1,33 @@
+<?php
+// Conectar a la base de datos de PhpMyAdmin
+$dsn = 'mysql:dbname=inscripcion;host=localhost';
+$usuario = 'root';
+$password = '';
+
+try {
+    $conexion = new PDO($dsn, $usuario, $password);
+} catch (PDOException $e) {
+    echo 'Error de conexión: ' . $e->getMessage();
+    exit();
+}
+
+// Obtener los valores enviados por el formulario
+$usuario = $_POST['usuario'];
+$password = $_POST['password'];
+
+// Crear la consulta SQL para buscar el usuario y el password en la tabla "usuarios"
+$consulta = "SELECT * FROM usuarios WHERE usuario = :usuario AND password = :password";
+$statement = $conexion->prepare($consulta);
+$statement->execute(array(':usuario' => $usuario, ':password' => $password));
+$resultado = $statement->fetch(PDO::FETCH_ASSOC);
+
+// Verificar si se encontró algún registro en la tabla
+if ($resultado) {
+    // Si el usuario y el password son correctos, redireccionar a la página de inicio
+    header('Location: inicio.php');
+    exit;
+} else {
+    // Si el usuario o el password son incorrectos, mostrar un mensaje de error
+    echo "Usuario o contraseña incorrectos";
+}
+?>
